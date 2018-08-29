63557af07c645cda2c59db60efe5a345a39286ea
Anwesh43 <anweshthecool0@gmail.com>
2018-08-30 02:27:44 +0530

    adding LinkeLineAltBar class

--- linealtbarview/src/main/java/com/anwesh/uiprojects/linealtbarview/LineAltBarView.kt
+++ linealtbarview/src/main/java/com/anwesh/uiprojects/linealtbarview/LineAltBarView.kt
@@ -142,4 +142,26 @@ class LineAltBarView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedLineAltBar(var i : Int) {
+        private var curr : LABNode = LABNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+    }
 }
