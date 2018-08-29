ba5d2a16ad2885f7a2a13c8f54be475264dd229d
Anwesh43 <anweshthecool0@gmail.com>
2018-08-30 02:25:25 +0530

    adding LABNode class

--- linealtbarview/src/main/java/com/anwesh/uiprojects/linealtbarview/LineAltBarView.kt
+++ linealtbarview/src/main/java/com/anwesh/uiprojects/linealtbarview/LineAltBarView.kt
@@ -99,4 +99,47 @@ class LineAltBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LABNode(var i : Int, val state : State = State()) {
+        var prev : LABNode? = this
+        var next : LABNode? = this
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LABNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLABNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LABNode {
+            var curr : LABNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
