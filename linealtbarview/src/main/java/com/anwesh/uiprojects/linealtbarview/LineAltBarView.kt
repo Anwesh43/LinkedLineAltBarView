41e66565b155e6d5a9999894da8a770594a55df6
Anwesh43 <anweshthecool0@gmail.com>
2018-08-30 02:06:04 +0530

    adding libraries

--- linealtbarview/src/main/java/com/anwesh/uiprojects/linealtbarview/LineAltBarView.kt
+++ linealtbarview/src/main/java/com/anwesh/uiprojects/linealtbarview/LineAltBarView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.linealtbarview
 /**
  * Created by anweshmishra on 30/08/18.
  */
+
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.RectF
+
+val nodes : Int = 5
