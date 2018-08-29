2b590e0212107098be813ce4c071c6b54a3e30b2
Anwesh43 <anweshthecool0@gmail.com>
2018-08-30 02:21:10 +0530

    adding Animator class

--- linealtbarview/src/main/java/com/anwesh/uiprojects/linealtbarview/LineAltBarView.kt
+++ linealtbarview/src/main/java/com/anwesh/uiprojects/linealtbarview/LineAltBarView.kt
@@ -71,4 +71,32 @@ class LineAltBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
